07659b9a4d77fa9425c50ef79325504eaa77e4e3
xpall <127076043+xpall@users.noreply.github.com>
2023-08-16 01:11:09 +0800

    Operations function still not fixed

--- index.js
+++ index.js
@@ -2,46 +2,63 @@ const operand = document.querySelectorAll('button.operand');
 const operation  = document.querySelectorAll('button.operation');
 const display = document.getElementById('display');
 
-let currentOperand = '';
-let tempAnswerContainer = '';
+let displayNumbers = '';
+let tailNumber = '';
+let headNumber = '';
+let tailOperator = '';
+let headOperator = '';
 
+// (1) for every numpress, save the digit
 operand.forEach(operand => {
   operand.addEventListener('click', e => {
-    // detect and get digit
     numKey = e['target']['textContent'];
-    console.log(`${numKey} is ${(typeof numKey)}`);
-    // save digit or digits
-    currentOperand += numKey;
-    // display digit or digits
-    display.textContent = currentOperand;
+    displayNumbers += numKey;
+    display.textContent = displayNumbers;
   })
 });
 
+// (2) once an operator is pressed,
 operation.forEach(operation => {
   operation.addEventListener('click', e => {
-    // detect and get the operator symbol
-    operator = e.target.textContent;
-    // save and reset firstOperand
-    previousValue = Number(currentOperand);
-    currentOperand = '';
-    
-    // delegate operation
-    if (operator === '+') {
-      getSum(previousValue);
-    }
+    // make a copy of displayNumbers
+    headNumber = displayNumbers;
+    // reset display values
+    displayNumbers = '';
+    // do the previous operation, except for the very first operation (if no tailNumber)
+    // this if statement tests this
+    if (tailOperator === '' && headOperator === '') {
+      tailOperator = e['target']['textContent'];
+    } else if (tailOperator != '' && headOperator === '') {
+      headOperator = e['target']['textContent'];
+    } else {
+      tailOperator = headOperator;
+      headOperator = e['target']['textContent'];
+    };
+    // throw values to correct functions
+    if (tailOperator === '+') {
+      getSum(Number(headNumber), Number(tailNumber));
+    } else if (tailOperator === '-') {
+      getDiff(Number(headNumber), Number(tailNumber));
+    } else if (tailOperator === '*') {
+      getProd(Number(headNumber), Number(tailNumber));
+    };
   })
-})
+});
 
-function getSum(x) {
-  console.log(x + Number(currentOperand))
-}
+function getSum(a, b = 0) {
+  sum = b + a;
+  tailNumber = sum;
+  display.textContent = tailNumber;
+};
 
+function getDiff(a, b = 0) {
+  diff = b - a;
+  tailNumber = diff;
+  display.textContent = tailNumber;
+};
 
-// limit number to 10digits
-
-
-// when clicked an operation, 
-
-//   immediately evaluate previous operations
-
-// update display value
+function getProd(a, b = 1) {
+  prod = b * a;
+  tailNumber = prod;
+  display.textContent = tailNumber;
+};
